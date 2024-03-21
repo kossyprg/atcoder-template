9d2c5d7bd12e0a29ab54eb740930224ac6553b5e
kossyprg <60993969+kossyprg@users.noreply.github.com>
2024-03-21 21:24:48 +0900

    add wsl-config

---
+++ wsl-config/.bash_aliases
@@ -0,0 +1,3 @@
+alias a='./a.out'
+alias g="g++ -O0 -std=gnu++17 -Wall -Wextra -Wshadow -Wfloat-equal -I ~/atcoder/ac-library"
+alias start='bash ~/start.sh'
---
+++ wsl-config/start.sh
@@ -0,0 +1,7 @@
+cp -n ~/atcoder/template.cpp a.cpp
+cp -n ~/atcoder/template.cpp b.cpp
+cp -n ~/atcoder/template.cpp c.cpp
+cp -n ~/atcoder/template.cpp d.cpp
+cp -n ~/atcoder/template.cpp e.cpp
+cp -n ~/atcoder/template.cpp f.cpp
+cp -n ~/atcoder/template.cpp g.cpp
